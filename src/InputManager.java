f0a04bdba51409bf933d60acdfeb336a15a68102
PierreLeidbring <pierre.leidbring@gmail.com>
2016-05-30 10:43:09 +0200

    Connection in inputManage
    Methods for establishing and closing connection

--- src/InputManager.java
+++ src/InputManager.java
@@ -1,6 +1,7 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * Created by Pierre on 2016-05-19.
@@ -8,11 +9,26 @@ import java.sql.SQLException;
 public class InputManager {
 
     private Connection connection;
+    private Statement stmt;
 
     public InputManager() {
+        initiateConnection();
+    }
+
+    private void initiateConnection() {
         try {
             connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/DB?user=root&password=myPassword");
+            stmt = connection.createStatement();
         }catch(SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void closeConnection() {
+        try {
+            connection.close();
+            stmt.close();
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
