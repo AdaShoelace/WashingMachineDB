efc9a77950a0ee947fd3d2216b332bf6ad8692d9
PierreLeidbring <pierre.leidbring@gmail.com>
2016-05-30 16:25:21 +0200

    Querys added

--- src/InputManager.java
+++ src/InputManager.java
@@ -30,6 +30,12 @@ public final class InputManager {
         }
     }
 
+    public void addCustomer() {
+
+    }
+
+
+
     public static String searchItem(String inputValue) {
 
         String query = "SELECT * FROM ITEM WHERE EAN=" + inputValue;
@@ -52,9 +58,46 @@ public final class InputManager {
         return res;
     }
 
-    public static void addItem(String inputValue) {
+    public String searchManufacturer(String manufacturer) {
+        String query = "SELECT * FROM ITEM WHERE Manufacturer=" + manufacturer;
+        String res = "";
+        try {
+            PreparedStatement stmt = conn.prepareStatement(query);
+            ResultSet rs = stmt.executeQuery();
+            while(rs.next()) {
+                res = "EAN: " + rs.getString(1) + "\n"
+                        + "Numbers in stock: " + rs.getString(2) +" \n"
+                        + "Model: " + rs.getString(3) + "\n"
+                        + "Manufacturer: " + rs.getString(4) + "\n"
+                        + "Price: " + rs.getString(5) + ":-" + "\n"
+                        + "Energyclass: " + rs.getString(8) + "\n"
+                        + "Description: " + rs.getString(9);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return res;
+    }
 
-        String query = "SELECT * FROM ITEM WHERE EAN=" + inputValue;
+    public static void addItem(String[] inputValue) {
+
+        String query = "insert into item values(" + inputValue[0] +
+                "," + inputValue[1] + "," + inputValue[2] + "," + inputValue[3] +
+                "," + inputValue[4] + "," + inputValue[5] + "," + inputValue[6] +
+                "," + inputValue[7] + "," + inputValue[8]+")";
+
+        try {
+            PreparedStatement stmt = conn.prepareStatement(query);
+            ResultSet rs = stmt.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void addCustomer(String[] inputValue) {
+
+        String query = "insert into customer values(" + inputValue[0] +
+                "," + inputValue[1] + "," + inputValue[2] + "," + inputValue[3] + ")";
 
         try {
             PreparedStatement stmt = conn.prepareStatement(query);
